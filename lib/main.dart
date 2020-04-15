6d13894796566a1c4796ea1fb74f46b7aed9ff62
emreerkmen <emre.erkmen@hotmail.com>
2020-04-15 22:39:15 +0300

    lesson 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+//type "stless" and push tab button to create a stateless class. it's very usefull key-short.
+//Hot reload can used with statefull widget.
+//Whenever we change the a widget, flutter look nearest build funk. and recall it. So all of widgets in this build are rebuild and these changes the screen.
+//Hotreload doesn't change current state of app. Fantastic.
+//Hotrestart change the current state to the start point.
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
