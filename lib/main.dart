c57c8595a6c73a4e75053d53e21eba7a54270fae
emreerkmen <emre.erkmen@hotmail.com>
2020-04-18 21:08:09 +0300

    before card lessons

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 //type "stless" and push tab button to create a stateless class. it's very usefull key-short.
 //Hot reload can used with statefull widget.
-//Whenever we change the a widget, flutter look nearest build funk. and recall it. So all of widgets in this build are rebuild and these changes the screen.
+//Whenever we change a widget, flutter look nearest build funk. and recall it. So all of widgets in this build are rebuild and these changes the screen.
 //Hotreload doesn't change current state of app. Fantastic.
 //Hotrestart change the current state to the start point.
 class MyApp extends StatelessWidget {
@@ -14,52 +14,90 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue[400],
         body: SafeArea(
-          child: Row(
-            //mainAxisSize: MainAxisSize.min,
-            //verticalDirection: VerticalDirection.up,
-            //mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: 100.0, //doesn't work in row with strerch
-                //margin: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-                //padding: EdgeInsets.all(10.0),
-                color: Colors.red,
-                child: Text('Ben'),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    //margin: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-                    //padding: EdgeInsets.all(10.0),
-                    color: Colors.yellow,
-                    child: Text('Erkmen'),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('images/avatar.jpg'),
+                ),
+                Text(
+                  'Emre Erkmen',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Container(
-                    width: 100.0,
-                    height: 100.0,
-                    //margin: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-                    //padding: EdgeInsets.all(10.0),
-                    color: Colors.yellow[100],
-                    child: Text('Erkmen'),
+                ),
+                Text(
+                  'SOFTWARE ENGINEER',
+                  style: TextStyle(
+                    fontFamily: 'Chewy',
+                    color: Colors.blue.shade800,
+                    fontSize: 30.0,
+                    //fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
                   ),
-                ],
-              ),
-              Container(
-                width: 100.0,
-                //margin: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-                //padding: EdgeInsets.all(10.0),
-                color: Colors.blue,
-                child: Text('Erkmen'),
-              ),
-            ],
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        //size: 100.0,
+                        color: Colors.blue.shade900,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+90 534 392 07 15',
+                        style: TextStyle(
+                          fontFamily: 'Chewy',
+                          fontSize: 20.0,
+                          color: Colors.blue.shade900,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        //size: 100.0,
+                        color: Colors.blue.shade900,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'emre.erkmen@hotmail.com',
+                        style: TextStyle(
+                          fontFamily: 'Chewy',
+                          fontSize: 20.0,
+                          color: Colors.blue.shade900,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
